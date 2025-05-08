180c372ad97d1ed6ca1e378c8e67eb11e746382b
ArthurFreitasJardim <arthurfreitasjardim@gmail.com>
2025-05-08 03:39:58 -0300

    Corrige importação da página Home

--- Portfolio/Portfolio/src/App.tsx
+++ Portfolio/Portfolio/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from './pages/home';
+import { Home } from './pages/home/Index';
 import AOS from "aos";
 import "aos/dist/aos.css";
 
